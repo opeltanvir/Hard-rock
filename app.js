8e815b5d2213851f83e66ad441ed7b32b5fe14a7
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-24 23:17:59 +0600

    hard rock

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const searchSongs = () =>{
     const searchText = document.getElementById('search-field').value;
    const url = ` https://api.lyrics.ovh/suggest/${searchText}`
+//    Load data
    fetch(url)
    .then(res => res.json())
-   .then(data => console.log(data))
+   .then(data => displaySongs (data.data))
+}
+
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+
+    songs.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML =`
+                     <div class="col-md-9">
+                        <h3 class="lyrics-name">${song.title}</h3>
+                        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                    </div>
+                         <div class="col-md-3 text-md-right text-center">
+                        <button class="btn btn-success">Get Lyrics</button>
+                    </div>
+         `;
+        songContainer.appendChild(songDiv);
+    })
 }
