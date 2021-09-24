b4c5d17697feda04042e3f6d1f6f4cd22cb39a78
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-25 00:06:47 +0600

    hard rock

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const searchSongs = () =>{
+const searchSongs = async() =>{
     const searchText = document.getElementById('search-field').value;
    const url = ` https://api.lyrics.ovh/suggest/${searchText}`
 //    Load data
-   fetch(url)
-   .then(res => res.json())
-   .then(data => displaySongs (data.data))
+    const res = await fetch(url);
+    const data = await res.json();
+    displaySongs(data.data);
 }
 
 const displaySongs = songs => {
     const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
 
     songs.forEach(song => {
         const songDiv = document.createElement('div');
@@ -17,11 +18,27 @@ const displaySongs = songs => {
                      <div class="col-md-9">
                         <h3 class="lyrics-name">${song.title}</h3>
                         <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                        <audio controls>
+                            <source src="horse.ogg" type="audio/ogg">
+                            <source src="${song.preview}" type="audio/mpeg">
+                        </audio>
                     </div>
                          <div class="col-md-3 text-md-right text-center">
-                        <button class="btn btn-success">Get Lyrics</button>
+                        <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
                     </div>
          `;
         songContainer.appendChild(songDiv);
     })
+}
+
+const getLyric = async (artist, title) => {
+        const url = ` https://api.lyrics.ovh/v1/${artist}/${title}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        displayLyrics(data.lyrics);
+}
+
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('song-lyrics')
+    lyricsDiv.innerText = lyrics;
 }
